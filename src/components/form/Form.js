9218dab0393bb85550c1ade0350eb01d8b81846b
jaroslaw-filipiak <info@j-filipiak.pl>
2023-11-19 22:44:41 +0100

    step3 base markup

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,7 @@
 import FormHeader from './FormHeader';
 import Step1 from './steps/Step1';
 import Step2 from './steps/Step2';
+import Step3 from './steps/Step3';
 
 export default function Form() {
   return (
@@ -8,7 +9,7 @@ export default function Form() {
       <div className='pt-5 d-flex flex-col items-between justify-between'>
         <FormHeader />
         {/* <Step1 /> */}
-        <Step2 />
+        <Step3 />
       </div>
     </>
   );
---
+++ src/components/form/steps/Step3.js
@@ -0,0 +1,91 @@
+export default function Step3() {
+  return (
+    <>
+      <section>
+        <div className='step--wrapper step-2 bg-[#f7f5f5]  relative'>
+          {/* label absolute */}
+          <div class='absolue inline-flex left-0 top-0 bg-main pt-3 pb-3 pl-8 pr-8 text-white font-bold text-base'>
+            Rodzaj podparcia
+          </div>
+          {/* content + padding */}
+          <div className='step--inner pt-20 pb-20 pl-10 pr-10 lg:w-8/12 mx-auto'>
+            <div className='flex flex-wrap items-start justify-center'>
+              {/* items */}
+              <div className='w-6/12 border flex flex-col items-center justify-center p-5'>
+                <label for='type1'>
+                  <img
+                    className='max-w-full'
+                    src='/assets/type-1.png'
+                    role='presentation'
+                  />
+                  <input
+                    type='radio'
+                    id='type1'
+                    name='supportType'
+                    value='type1'
+                  />
+                </label>
+              </div>
+
+              <div className='w-6/12 border flex flex-col items-center justify-center p-5'>
+                <label for='type2'>
+                  <img
+                    className='max-w-full'
+                    src='/assets/type-2.png'
+                    role='presentation'
+                  />
+                  <input
+                    type='radio'
+                    id='type2'
+                    name='supportType'
+                    value='type2'
+                  />
+                </label>
+              </div>
+
+              <div className='w-6/12 border flex flex-col items-center justify-center p-5'>
+                <label for='type3'>
+                  <img
+                    className='max-w-full'
+                    src='/assets/type-3.png'
+                    role='presentation'
+                  />
+                  <input
+                    type='radio'
+                    id='type3'
+                    name='supportType'
+                    value='type3'
+                  />
+                </label>
+              </div>
+
+              <div className='w-6/12 border flex flex-col items-center justify-center p-5'>
+                <label for='type4'>
+                  <img
+                    className='max-w-full'
+                    src='/assets/type-4.png'
+                    role='presentation'
+                  />
+                  <input
+                    type='radio'
+                    id='type4'
+                    name='supportType'
+                    value='type4'
+                  />
+                </label>
+              </div>
+            </div>
+
+            {/* mobile btn */}
+            <div class='w-full flex items-center justify-center mt-20 mb-16'>
+              <button className='btn btn--main btn--rounded'>
+                Następny krok
+                <img className='ml-5' src='/assets/arrow-next.svg' alt='' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
