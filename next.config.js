15e739d230ed58c650ee4788011da4c817109c8b
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-02 10:46:04 +0200

    set images to unoptimized

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/clients/ddgro-api/form',
-  assetPrefix: '/clients/ddgro-api/form/',
+  // basePath: !isProd ? '/clients/ddgro-api/form' : undefined,
+  // assetPrefix: !isProd ? '/clients/ddgro-api/form/' : undefined,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
