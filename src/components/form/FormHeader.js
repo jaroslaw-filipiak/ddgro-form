f442aa51631090793fb6f283de07b3ab51d23019
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-02 10:25:06 +0200

    assetPrefix to slash

--- src/components/form/FormHeader.js
+++ src/components/form/FormHeader.js
@@ -12,7 +12,7 @@ export default function FormHeader({
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('check asset prefix');
+    console.log('assetPrefix to slash');
     fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/accesories`)
       .then((response) => response.json())
       .then((data) => {
