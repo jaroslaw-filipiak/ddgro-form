05a7e6a03faf62e168333a3983c8c8f26ca65fa7
jaroslaw-filipiak <info@j-filipiak.pl>
2024-07-01 21:44:10 +0200

    Update m_max matrix

--- src/components/form/steps/Step7.js
+++ src/components/form/steps/Step7.js
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   calculateHowManyTitlesCanFillTheSquare,
   calculateSupportsCount,
@@ -21,9 +21,9 @@ import {
   setAverageInEachSectionRaptor,
   setM_RAPTOR,
   setM_RAPTOR_ORDER,
-} from '@/store/slices/formSlice';
+} from "@/store/slices/formSlice";
 
-import Image from 'next/image';
+import Image from "next/image";
 
 function capitalizeFirstLetter(string) {
   if (string) {
@@ -63,20 +63,20 @@ export default function Step7({ setFormAsideVisibility }) {
   // columns for M_STANDARD and other MATRIX OBJ
   const columns = [
     {
-      key: 'wys_mm',
-      label: 'wys_mm',
+      key: "wys_mm",
+      label: "wys_mm",
     },
     {
-      key: 'range',
-      label: 'przedział',
+      key: "range",
+      label: "przedział",
     },
     {
-      key: 'condition',
-      label: 'warunek',
+      key: "condition",
+      label: "warunek",
     },
     {
-      key: 'count_in_range',
-      label: 'ilość w predziale',
+      key: "count_in_range",
+      label: "ilość w predziale",
     },
   ];
 
@@ -104,30 +104,30 @@ export default function Step7({ setFormAsideVisibility }) {
     const calculateRange = (item) => {
       switch (true) {
         case item.wys_mm >= 10 && item.wys_mm <= 16:
-          return '10-17';
+          return "10-17";
         case item.wys_mm >= 17 && item.wys_mm <= 29:
-          return '17-30';
+          return "17-30";
         case item.wys_mm >= 30 && item.wys_mm <= 45:
-          return '30-45';
+          return "30-45";
         case item.wys_mm >= 46 && item.wys_mm <= 69:
-          return '45-70';
+          return "45-70";
         case item.wys_mm >= 70 && item.wys_mm <= 119:
-          return '70-120';
+          return "70-120";
         case item.wys_mm >= 120 && item.wys_mm <= 219:
-          return '120-220';
+          return "120-220";
         case item.wys_mm >= 220 && item.wys_mm <= 319:
-          return '220-320';
+          return "220-320";
         case item.wys_mm >= 320 && item.wys_mm <= 420:
-          return '320-420';
+          return "320-420";
         case item.wys_mm >= 421 && item.wys_mm <= 549:
-          return '350-550';
+          return "350-550";
         case item.wys_mm >= 550 && item.wys_mm <= 749:
-          return '550-750';
+          return "550-750";
         case item.wys_mm >= 750 && item.wys_mm <= 950:
-          return '750-950';
+          return "750-950";
 
         default:
-          return '';
+          return "";
       }
     };
 
@@ -166,39 +166,39 @@ export default function Step7({ setFormAsideVisibility }) {
     const calculateRange = (item) => {
       switch (true) {
         case item.wys_mm >= 10 && item.wys_mm <= 16:
-          return '10-17';
+          return "10-17";
         case item.wys_mm >= 17 && item.wys_mm <= 29:
-          return '17-30';
+          return "17-30";
         case item.wys_mm >= 30 && item.wys_mm <= 49:
-          return '30-50';
+          return "30-50";
         case item.wys_mm >= 50 && item.wys_mm <= 69:
-          return '50-70';
+          return "50-70";
         case item.wys_mm >= 70 && item.wys_mm <= 89:
-          return '70-90';
+          return "70-90";
         case item.wys_mm >= 90 && item.wys_mm <= 109:
-          return '90-110';
+          return "90-110";
         case item.wys_mm >= 110 && item.wys_mm <= 129:
-          return '110-130';
+          return "110-130";
         case item.wys_mm >= 130 && item.wys_mm <= 149:
-          return '130-150';
+          return "130-150";
         case item.wys_mm >= 150 && item.wys_mm <= 169:
-          return '150-170';
+          return "150-170";
         case item.wys_mm >= 170 && item.wys_mm <= 189:
-          return '170-190';
+          return "170-190";
         case item.wys_mm >= 190 && item.wys_mm <= 209:
-          return '190-210';
+          return "190-210";
         case item.wys_mm >= 220 && item.wys_mm <= 319:
-          return '220-320';
+          return "220-320";
         case item.wys_mm >= 320 && item.wys_mm <= 419:
-          return '320-420';
+          return "320-420";
         case item.wys_mm >= 350 && item.wys_mm <= 549:
-          return '350-550';
+          return "350-550";
         case item.wys_mm >= 550 && item.wys_mm <= 749:
-          return '550-750';
+          return "550-750";
         case item.wys_mm >= 750 && item.wys_mm <= 950:
-          return '750-950';
+          return "750-950";
         default:
-          return '';
+          return "";
       }
     };
 
@@ -238,26 +238,26 @@ export default function Step7({ setFormAsideVisibility }) {
     const calculateRange = (item) => {
       switch (true) {
         case item.wys_mm >= 10 && item.wys_mm <= 16:
-          return '10-17';
+          return "10-17";
         case item.wys_mm >= 17 && item.wys_mm <= 29:
-          return '17-30';
-        case item.wys_mm >= 30 && item.wys_mm <= 49:
-          return '30-50';
+          return "17-30";
+        case item.wys_mm >= 30 && item.wys_mm <= 44:
+          return "30-50";
         case item.wys_mm >= 45 && item.wys_mm <= 74:
-          return '45-75';
+          return "45-75";
         case item.wys_mm >= 75 && item.wys_mm <= 149:
-          return '75-150';
+          return "75-150";
         case item.wys_mm >= 150 && item.wys_mm <= 349:
-          return '150-350';
+          return "150-350";
         case item.wys_mm >= 350 && item.wys_mm <= 549:
-          return '350-550';
+          return "350-550";
         case item.wys_mm >= 550 && item.wys_mm <= 749:
-          return '550-750';
+          return "550-750";
         case item.wys_mm >= 750 && item.wys_mm <= 950:
-          return '750-950';
+          return "750-950";
 
         default:
-          return '';
+          return "";
       }
     };
 
@@ -297,26 +297,26 @@ export default function Step7({ setFormAsideVisibility }) {
     const calculateRange = (item) => {
       switch (true) {
         case item.wys_mm >= 10 && item.wys_mm <= 16:
-          return '10-17';
+          return "10-17";
         case item.wys_mm >= 17 && item.wys_mm <= 29:
-          return '17-30';
+          return "17-30";
         case item.wys_mm >= 30 && item.wys_mm <= 49:
-          return '30-50';
+          return "30-50";
         case item.wys_mm >= 45 && item.wys_mm <= 74:
-          return '45-75';
+          return "45-75";
         case item.wys_mm >= 75 && item.wys_mm <= 149:
-          return '75-150';
+          return "75-150";
         case item.wys_mm >= 150 && item.wys_mm <= 349:
-          return '150-350';
+          return "150-350";
         case item.wys_mm >= 350 && item.wys_mm <= 549:
-          return '350-550';
+          return "350-550";
         case item.wys_mm >= 550 && item.wys_mm <= 749:
-          return '550-750';
+          return "550-750";
         case item.wys_mm >= 750 && item.wys_mm <= 950:
-          return '750-950';
+          return "750-950";
 
         default:
-          return '';
+          return "";
       }
     };
 
@@ -592,16 +592,16 @@ export default function Step7({ setFormAsideVisibility }) {
 
   const matrixCalculate = () => {
     try {
-      console.log('creating matrix M_STANDARD MATRIX');
+      console.log("creating matrix M_STANDARD MATRIX");
       M_STANDARD();
-      console.log('creating matrix M_SPIRAL MATRIX');
+      console.log("creating matrix M_SPIRAL MATRIX");
       M_SPIRAL();
-      console.log('creating matrix M_MAX');
+      console.log("creating matrix M_MAX");
       M_MAX();
-      console.log('creating matrix M_RAPTOR');
+      console.log("creating matrix M_RAPTOR");
       M_RAPTOR();
     } catch (error) {
-      console.log('problem with creating matrix...');
+      console.log("problem with creating matrix...");
       console.log(error);
     }
   };
@@ -623,7 +623,7 @@ export default function Step7({ setFormAsideVisibility }) {
 
   function finalCalculate() {
     // dispatch for recaltulcate
-    console.log('final calculate..');
+    console.log("final calculate..");
 
     dispatch(calculateLA());
     dispatch(calculateHowManyTitlesCanFillTheSquare());
@@ -637,135 +637,135 @@ export default function Step7({ setFormAsideVisibility }) {
   return (
     <>
       <section>
-        <div className='step--wrapper step-7 bg-[#f7f5f5]  relative'>
+        <div className="step--wrapper step-7 bg-[#f7f5f5]  relative">
           {/* label absolute */}
-          <div className='absolue inline-flex left-0 top-0  text-white font-bold text-bas flex-col gap-1 items-start justify-center'>
-            <p className='bg-main pt-3 pb-3 pl-8 pr-8'>Podsumowanie</p>
+          <div className="absolue inline-flex left-0 top-0  text-white font-bold text-bas flex-col gap-1 items-start justify-center">
+            <p className="bg-main pt-3 pb-3 pl-8 pr-8">Podsumowanie</p>
           </div>
           {/* content + padding */}
-          <div className='step--inner pt-20 pb-20 lg:pl-10 lg:pr-10 lg:w-10/12 mx-auto'>
+          <div className="step--inner pt-20 pb-20 lg:pl-10 lg:pr-10 lg:w-10/12 mx-auto">
             <div>
-              <p className='text-2xl font-bold text-black text-opacity-70 pb-9'>
+              <p className="text-2xl font-bold text-black text-opacity-70 pb-9">
                 Wprowadzone parametry
               </p>
-              <ul className='flex flex-wrap items-center justify-start gap-6'>
+              <ul className="flex flex-wrap items-center justify-start gap-6">
                 {state?.type && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Rodzaj nawierzchni:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
-                      {state.type === 'wood' ? 'Deski' : 'Płyty'}
+                    <p className="text-lg text-black text-opacity-70 font-bold">
+                      {state.type === "wood" ? "Deski" : "Płyty"}
                     </p>
                   </li>
                 )}
 
                 {state?.main_system && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Wybrany główny system:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
+                    <p className="text-lg text-black text-opacity-70 font-bold">
                       {capitalizeFirstLetter(state?.main_system)}
                     </p>
                   </li>
                 )}
 
                 {state?.total_area && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Łączna powierzchnia:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
+                    <p className="text-lg text-black text-opacity-70 font-bold">
                       {state?.total_area} m2
                     </p>
                   </li>
                 )}
 
                 {state.count && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Ilość tarasów:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
+                    <p className="text-lg text-black text-opacity-70 font-bold">
                       {state?.count}
                     </p>
                   </li>
                 )}
 
                 {state?.lowest && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Najniższy punkt wysokości tarasu:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
+                    <p className="text-lg text-black text-opacity-70 font-bold">
                       {state?.lowest} mm
                     </p>
                   </li>
                 )}
 
                 {state?.highest && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Najwyższy punkt wysokości tarasu:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
+                    <p className="text-lg text-black text-opacity-70 font-bold">
                       {state?.highest} mm
                     </p>
                   </li>
                 )}
 
                 {state?.additional_accessories && (
-                  <li className='flex items-center justify-start gap-2'>
-                    <p className=' text-lg text-black text-opacity-50 font-normal'>
+                  <li className="flex items-center justify-start gap-2">
+                    <p className=" text-lg text-black text-opacity-50 font-normal">
                       Czy wybrano dodatkowe akcesoria:
                     </p>
-                    <p className='text-lg text-black text-opacity-70 font-bold'>
-                      {state?.additional_accessories ? 'Tak' : 'Nie'}
+                    <p className="text-lg text-black text-opacity-70 font-bold">
+                      {state?.additional_accessories ? "Tak" : "Nie"}
                     </p>
                   </li>
                 )}
               </ul>
             </div>
 
-            <div className='summary mt-20 pb-9'>
-              <p className='text-2xl font-bold text-black text-opacity-70 pb-9'>
+            <div className="summary mt-20 pb-9">
+              <p className="text-2xl font-bold text-black text-opacity-70 pb-9">
                 Wynik:
               </p>
 
-              <div className='square--wrapper'></div>
+              <div className="square--wrapper"></div>
 
               <ul>
-                <li className='flex items-center justify-between border-b border-black border-opacity-50 p-6'>
-                  <p className='text-xl text-black text-opacity-50 font-normal'>
+                <li className="flex items-center justify-between border-b border-black border-opacity-50 p-6">
+                  <p className="text-xl text-black text-opacity-50 font-normal">
                     Wsporników:
                   </p>
-                  <div className='flex items-center'>
-                    <p className='text-black text-opacity-50 text-base pl-4'>
-                      {state?.supports_count ? state?.supports_count : '-'}
+                  <div className="flex items-center">
+                    <p className="text-black text-opacity-50 text-base pl-4">
+                      {state?.supports_count ? state?.supports_count : "-"}
                     </p>
                   </div>
                 </li>
 
                 <li
                   className={`flex items-center justify-between border-b border-black border-opacity-50 p-6 ${
-                    state.type === 'wood' ? 'hidden' : ''
+                    state.type === "wood" ? "hidden" : ""
                   }`}
                 >
                   <p
                     className={`text-xl text-black text-opacity-50 font-normal ${
-                      state.type === 'wood' ? 'hidden' : ''
+                      state.type === "wood" ? "hidden" : ""
                     }`}
                   >
                     Liczba płyt:
                   </p>
                   <div
                     className={`flex items-center ${
-                      state.type === 'wood' ? 'hidden' : ''
+                      state.type === "wood" ? "hidden" : ""
                     }`}
                   >
-                    <p className='text-black text-opacity-50 text-base pl-4'>
-                      {state?.slabs_count ? state?.slabs_count : '-'}
+                    <p className="text-black text-opacity-50 text-base pl-4">
+                      {state?.slabs_count ? state?.slabs_count : "-"}
                     </p>
                   </div>
                 </li>
@@ -780,21 +780,21 @@ export default function Step7({ setFormAsideVisibility }) {
             </button> */}
 
             {/* mobile btn */}
-            <div className='w-full flex items-center justify-center mt-20 mb-16'>
+            <div className="w-full flex items-center justify-center mt-20 mb-16">
               <button
                 onClick={() => {
                   handleCalculate();
                   setFormAsideVisibility(true);
                 }}
-                className='btn btn--main btn--rounded'
+                className="btn btn--main btn--rounded"
               >
                 Odbierz PDF
                 <Image
                   width={42}
                   height={42}
-                  className='ml-5'
-                  src='/assets/arrow-next.svg'
-                  alt='arrow next'
+                  className="ml-5"
+                  src="/assets/arrow-next.svg"
+                  alt="arrow next"
                 />
               </button>
             </div>
