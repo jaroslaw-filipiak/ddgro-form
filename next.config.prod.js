97324d38cb95670931b6d77e10123e451febb1cd
jaroslaw-filipiak <info@j-filipiak.pl>
2024-07-14 14:53:45 +0200

    next.config settings

--- next.config.prod.js
+++ next.config.prod.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/",
-  assetPrefix: "/",
+  basePath: "",
+  assetPrefix: "",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
