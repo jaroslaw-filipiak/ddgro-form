b6c398876d5eea541676734063354dffb70c7522
jaroslaw-filipiak <info@j-filipiak.pl>
2025-03-16 16:03:47 +0100

    add generateStaticParams

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,11 @@
-'use client'
-
 import Form from '@/components/form/Form'
 
-export default function HomePage() {
+// Define the generateStaticParams function to support static export
+export function generateStaticParams() {
+    return ['en', 'fr', 'pl'].map(locale => ({ locale }))
+}
+
+export default function Page() {
     return (
         <main className='container min-h-screen'>
             <Form />
